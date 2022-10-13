1f2d13cb2b93997089a077857f2558da0ec4b2d7
WebDev717 <blackhorse.cryptodev@gmail.com>
2022-10-13 11:42:52 -0700

    add safari3 api route

--- server.js
+++ server.js
@@ -201,8 +201,7 @@ app.get("/works-in-chrome-and-safari", (req, res) => {
   );
 });
 
-app.get('/works-in-chrome-and-safari1', (req, res) => {
-
+app.get("/works-in-chrome-and-safari1", (req, res) => {
   // Listing 3.
   const options = {};
 
@@ -211,84 +210,81 @@ app.get('/works-in-chrome-and-safari1', (req, res) => {
 
   const range = req.headers.range;
   if (range) {
-      const bytesPrefix = "bytes=";
-      if (range.startsWith(bytesPrefix)) {
-          const bytesRange = range.substring(bytesPrefix.length);
-          const parts = bytesRange.split("-");
-          if (parts.length === 2) {
-              const rangeStart = parts[0] && parts[0].trim();
-              if (rangeStart && rangeStart.length > 0) {
-                  options.start = start = parseInt(rangeStart);
-              }
-              const rangeEnd = parts[1] && parts[1].trim();
-              if (rangeEnd && rangeEnd.length > 0) {
-                  options.end = end = parseInt(rangeEnd);
-              }
-          }
+    const bytesPrefix = "bytes=";
+    if (range.startsWith(bytesPrefix)) {
+      const bytesRange = range.substring(bytesPrefix.length);
+      const parts = bytesRange.split("-");
+      if (parts.length === 2) {
+        const rangeStart = parts[0] && parts[0].trim();
+        if (rangeStart && rangeStart.length > 0) {
+          options.start = start = parseInt(rangeStart);
+        }
+        const rangeEnd = parts[1] && parts[1].trim();
+        if (rangeEnd && rangeEnd.length > 0) {
+          options.end = end = parseInt(rangeEnd);
+        }
       }
+    }
   }
 
   res.setHeader("content-type", "video/mp4");
 
   fs.stat(filePath, (err, stat) => {
-      if (err) {
-          console.error(`File stat error for ${filePath}.`);
-          console.error(err);
-          res.sendStatus(500);
-          return;
+    if (err) {
+      console.error(`File stat error for ${filePath}.`);
+      console.error(err);
+      res.sendStatus(500);
+      return;
+    }
+
+    let contentLength = stat.size;
+
+    // Listing 4.
+    if (req.method === "HEAD") {
+      res.statusCode = 200;
+      res.setHeader("accept-ranges", "bytes");
+      res.setHeader("content-length", contentLength);
+      res.end();
+    } else {
+      // Listing 5.
+      let retrievedLength;
+      if (start !== undefined && end !== undefined) {
+        retrievedLength = end + 1 - start;
+      } else if (start !== undefined) {
+        retrievedLength = contentLength - start;
+      } else if (end !== undefined) {
+        retrievedLength = end + 1;
+      } else {
+        retrievedLength = contentLength;
       }
 
-      let contentLength = stat.size;
+      // Listing 6.
+      res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
 
-      // Listing 4.
-      if (req.method === "HEAD") {
-          res.statusCode = 200;
-          res.setHeader("accept-ranges", "bytes");
-          res.setHeader("content-length", contentLength);
-          res.end();
+      res.setHeader("content-length", retrievedLength);
+
+      if (range !== undefined) {
+        res.setHeader(
+          "content-range",
+          `bytes ${start || 0}-${end || contentLength - 1}/${contentLength}`
+        );
+        res.setHeader("accept-ranges", "bytes");
       }
-      else {       
-          // Listing 5.
-          let retrievedLength;
-          if (start !== undefined && end !== undefined) {
-              retrievedLength = (end+1) - start;
-          }
-          else if (start !== undefined) {
-              retrievedLength = contentLength - start;
-          }
-          else if (end !== undefined) {
-              retrievedLength = (end+1);
-          }
-          else {
-              retrievedLength = contentLength;
-          }
 
-          // Listing 6.
-          res.statusCode = start !== undefined || end !== undefined ? 206 : 200;
+      // Listing 7.
+      const fileStream = fs.createReadStream(filePath, options);
+      fileStream.on("error", (error) => {
+        console.log(`Error reading file ${filePath}.`);
+        console.log(error);
+        res.sendStatus(500);
+      });
 
-          res.setHeader("content-length", retrievedLength);
-
-          if (range !== undefined) {  
-              res.setHeader("content-range", `bytes ${start || 0}-${end || (contentLength-1)}/${contentLength}`);
-              res.setHeader("accept-ranges", "bytes");
-          }
-
-          // Listing 7.
-          const fileStream = fs.createReadStream(filePath, options);
-          fileStream.on("error", error => {
-              console.log(`Error reading file ${filePath}.`);
-              console.log(error);
-              res.sendStatus(500);
-          });
-
-
-          fileStream.pipe(res);
-      }
+      fileStream.pipe(res);
+    }
   });
 });
 
-app.get('/works-in-chrome-and-safari2', (req, res) => {
-
+app.get("/works-in-chrome-and-safari2", (req, res) => {
   // Listing 3.
   var s3 = new AWS.S3();
   const mimetype = "video/mp4";
@@ -299,21 +295,21 @@ app.get('/works-in-chrome-and-safari2', (req, res) => {
 
   const range = req.headers.range;
   if (range) {
-      const bytesPrefix = "bytes=";
-      if (range.startsWith(bytesPrefix)) {
-          const bytesRange = range.substring(bytesPrefix.length);
-          const parts = bytesRange.split("-");
-          if (parts.length === 2) {
-              const rangeStart = parts[0] && parts[0].trim();
-              if (rangeStart && rangeStart.length > 0) {
-                  options.start = start = parseInt(rangeStart);
-              }
-              const rangeEnd = parts[1] && parts[1].trim();
-              if (rangeEnd && rangeEnd.length > 0) {
-                  options.end = end = parseInt(rangeEnd);
-              }
-          }
+    const bytesPrefix = "bytes=";
+    if (range.startsWith(bytesPrefix)) {
+      const bytesRange = range.substring(bytesPrefix.length);
+      const parts = bytesRange.split("-");
+      if (parts.length === 2) {
+        const rangeStart = parts[0] && parts[0].trim();
+        if (rangeStart && rangeStart.length > 0) {
+          options.start = start = parseInt(rangeStart);
+        }
+        const rangeEnd = parts[1] && parts[1].trim();
+        if (rangeEnd && rangeEnd.length > 0) {
+          options.end = end = parseInt(rangeEnd);
+        }
       }
+    }
   }
 
   res.setHeader("content-type", "video/mp4");
@@ -331,9 +327,11 @@ app.get('/works-in-chrome-and-safari2', (req, res) => {
 
       console.log("made request to s3 to list objects");
       console.log(data);
+      console.log(req.header.range);
       if (req != null && req.headers.range != null) {
-        console.log(req);
+        // console.log(req);
         var range = req.headers.range;
+        console.log(range);
         var bytes = range.replace(/bytes=/, "").split("-");
         var start = parseInt(bytes[0], 10);
         var total = data.Contents[0].Size;
@@ -373,7 +371,7 @@ app.get('/works-in-chrome-and-safari2', (req, res) => {
           .pipe(res);
         console.log("got object from s3 and created readstream");
       } else {
-        console.log(req);
+        // console.log(req);
         res.writeHead(200, {
           "Cache-Control": "max-age=" + cache + ", private",
           "Content-Length": data.Contents[0].Size,
@@ -388,10 +386,119 @@ app.get('/works-in-chrome-and-safari2', (req, res) => {
           .pipe(res);
         console.log("fell into else statement, wrote header");
       }
-    })
-
+    }
+  );
 });
 
+app.get("/works-in-chrome-and-safari3", (req, res) => {
+  // Listing 3.
+  var s3 = new AWS.S3();
+  const mimetype = "video/mp4";
+  const cache = 0;
+  let start;
+  let end;
+  const options = {};
+
+  const range = req.headers.range;
+  if (range) {
+    const bytesPrefix = "bytes=";
+    if (range.startsWith(bytesPrefix)) {
+      const bytesRange = range.substring(bytesPrefix.length);
+      const parts = bytesRange.split("-");
+      if (parts.length === 2) {
+        const rangeStart = parts[0] && parts[0].trim();
+        if (rangeStart && rangeStart.length > 0) {
+          options.start = start = parseInt(rangeStart);
+        }
+        const rangeEnd = parts[1] && parts[1].trim();
+        if (rangeEnd && rangeEnd.length > 0) {
+          options.end = end = parseInt(rangeEnd);
+        }
+      }
+    }
+  }
+
+  res.setHeader("content-type", "video/mp4");
+
+  s3.listObjectsV2(
+    {
+      Bucket: "elasticbeanstalk-us-east-2-045749248414",
+      MaxKeys: 1,
+      Prefix: filename,
+    },
+    function (err, data) {
+      if (err) {
+        return res.status(400).send(err);
+      }
+
+      console.log("made request to s3 to list objects");
+      console.log(data);
+      console.log(req.header.range);
+      if (req != null && req.headers.range != null) {
+        // console.log(req);
+        s3.headObject(
+          {
+            Bucket: "elasticbeanstalk-us-east-2-045749248414",
+            Key: filename,
+          },
+          function (err, data) {
+            if (err) {
+              // an error occurred
+              console.error(err);
+              return res.status(500).send("returning err");
+            }
+
+            const videoSize = Number(data.ContentLength);
+            const CHUNK_SIZE = 10 ** 6; // 1MB
+            const start = Number(range.replace(/\D/g, ""));
+            const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+
+            var params = {
+              Bucket: "elasticbeanstalk-us-east-2-045749248414",
+              Key: filename,
+              Range: range,
+            };
+            var stream = s3.getObject(params).createReadStream();
+
+            res.status(206);
+            res.set("Content-Type", data.ContentType);
+            res.set("Content-Disposition", "inline");
+            res.set("Accept-Ranges", "bytes");
+            res.set("Accept-Encoding", "Identity");
+            res.set(
+              "Content-Range",
+              "bytes " + start + "-" + end + "/" + videoSize
+            );
+            res.set("Content-Length", data.ContentLength);
+            res.set(
+              "X-Playback-Session-Id",
+              req.header("X-Playback-Session-Id")
+            ); // Part of the attempt to fix
+            res.set("Connection", "keep-alive");
+            res.set("Last-Modified", data.LastModified);
+            res.set("ETag", data.ETag);
+            stream.pipe(res);
+          }
+        );
+      } else {
+        // console.log(req);
+        res.writeHead(200, {
+          "Cache-Control": "max-age=" + cache + ", private",
+          "Content-Length": data.Contents[0].Size,
+          "Last-Modified": data.Contents[0].LastModified,
+          "Content-Type": mimetype,
+        });
+        s3.getObject({
+          Bucket: "elasticbeanstalk-us-east-2-045749248414",
+          Key: filename,
+        })
+          .createReadStream()
+          .pipe(res);
+        console.log("fell into else statement, wrote header");
+      }
+    }
+  );
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
