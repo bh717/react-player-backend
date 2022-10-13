02cb2e60ca029f4492f3b4f891c272203e71ec27
WebDev717 <blackhorse.cryptodev@gmail.com>
2022-10-13 10:41:05 -0700

    add safari2 api

--- server.js
+++ server.js
@@ -26,7 +26,6 @@ AWS.config.getCredentials(function (err) {
 app.get("/api/annotate", (req, res) => {
   var s3 = new AWS.S3();
   const mimetype = "video/mp4";
-  // const file = "https://elasticbeanstalk-us-east-2-045749248414.s3.amazonaws.com/1665446699465";
   const file = "1662639479008.mp4";
   const cache = 0;
   console.log("const mimetype, file, and cache declared");
@@ -75,7 +74,6 @@ app.get("/api/annotate", (req, res) => {
           "Content-Type": mimetype,
           "Content-Disposition": "inline",
           Connection: "keep-alive",
-          ETag: data.Contents[0].ETag,
         });
         console.log("wrote header");
         s3.getObject({
@@ -289,6 +287,110 @@ app.get('/works-in-chrome-and-safari1', (req, res) => {
   });
 });
 
+app.get('/works-in-chrome-and-safari2', (req, res) => {
+
+  // Listing 3.
+  var s3 = new AWS.S3();
+  const mimetype = "video/mp4";
+  const cache = 0;
+  let start;
+  let end;
+  const options = {};
+
+  const range = req.headers.range;
+  if (range) {
+      const bytesPrefix = "bytes=";
+      if (range.startsWith(bytesPrefix)) {
+          const bytesRange = range.substring(bytesPrefix.length);
+          const parts = bytesRange.split("-");
+          if (parts.length === 2) {
+              const rangeStart = parts[0] && parts[0].trim();
+              if (rangeStart && rangeStart.length > 0) {
+                  options.start = start = parseInt(rangeStart);
+              }
+              const rangeEnd = parts[1] && parts[1].trim();
+              if (rangeEnd && rangeEnd.length > 0) {
+                  options.end = end = parseInt(rangeEnd);
+              }
+          }
+      }
+  }
+
+  res.setHeader("content-type", "video/mp4");
+
+  s3.listObjectsV2(
+    {
+      Bucket: "elasticbeanstalk-us-east-2-045749248414",
+      MaxKeys: 1,
+      Prefix: filename,
+    },
+    function (err, data) {
+      if (err) {
+        return res.status(400).send(err);
+      }
+
+      console.log("made request to s3 to list objects");
+      console.log(data);
+      if (req != null && req.headers.range != null) {
+        console.log(req);
+        var range = req.headers.range;
+        var bytes = range.replace(/bytes=/, "").split("-");
+        var start = parseInt(bytes[0], 10);
+        var total = data.Contents[0].Size;
+        var end = bytes[1] ? parseInt(bytes[1], 10) : total - 1;
+
+        var chunksize = end - start + 1;
+
+        if (start !== undefined && end !== undefined) {
+          chunksize = end + 1 - start;
+        } else if (start !== undefined) {
+          chunksize = total - start;
+        } else if (end !== undefined) {
+          chunksize = end + 1;
+        } else {
+          chunksize = total;
+        }
+
+        console.log("declared range, bytes, start, total, end, chunksize vars");
+
+        res.writeHead(206, {
+          "Content-Range": "bytes " + start + "-" + end + "/" + total,
+          "Accept-Ranges": "bytes",
+          "Content-Length": chunksize,
+          "Accept-Encoding": "Identity",
+          "Last-Modified": data.Contents[0].LastModified,
+          "Content-Type": mimetype,
+          "Content-Disposition": "inline",
+          Connection: "keep-alive",
+        });
+        console.log("wrote header");
+        s3.getObject({
+          Bucket: "elasticbeanstalk-us-east-2-045749248414",
+          Key: filename,
+          Range: range,
+        })
+          .createReadStream()
+          .pipe(res);
+        console.log("got object from s3 and created readstream");
+      } else {
+        console.log(req);
+        res.writeHead(200, {
+          "Cache-Control": "max-age=" + cache + ", private",
+          "Content-Length": data.Contents[0].Size,
+          "Last-Modified": data.Contents[0].LastModified,
+          "Content-Type": mimetype,
+        });
+        s3.getObject({
+          Bucket: "elasticbeanstalk-us-east-2-045749248414",
+          Key: filename,
+        })
+          .createReadStream()
+          .pipe(res);
+        console.log("fell into else statement, wrote header");
+      }
+    })
+
+});
 
 const PORT = process.env.PORT || 3000;
 
